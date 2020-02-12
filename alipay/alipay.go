6713dc273c09cd8e7140af237d057b1b07af2b21
andy <150haomengting@163.com>
2020-02-12 22:59:12 +0800

    fix bugs

--- alipay/alipay.go
+++ alipay/alipay.go
@@ -64,37 +64,37 @@ type (
 	}
 	// Notice notice
 	Notice struct {
-		NotifyTime        string  `json:"notify_time,omitempty" url:"notify_time,omitempty"`                 // 是 时间
-		NotifyType        string  `json:"notify_type,omitempty" url:"notify_type,omitempty"`                 // 是 类型
-		NotifyID          string  `json:"notify_id,omitempty" url:"notify_id,omitempty"`                     // 是 校验ID
-		APPID             string  `json:"appid,omitempty" url:"appid,omitempty"`                             // 是 应用ID
-		Charset           string  `json:"charset,omitempty" url:"charset,omitempty"`                         // 是 utf-8
-		Version           string  `json:"version,omitempty" url:"version,omitempty"`                         // 是 1.0
-		SignType          string  `json:"sign_type,omitempty" url:"-"`                                       // 是 RSA2
-		Sign              string  `json:"sign,omitempty" url:"-"`                                            // 是 签名
-		TradeNo           string  `json:"trade_no,omitempty" url:"trade_no,omitempty"`                       // 是 交易号
-		OutTradeNo        string  `json:"out_trade_no,omitempty" url:"out_trade_no,omitempty"`               // 是 单号
-		OutBizNo          string  `json:"out_biz_no,omitempty" url:"out_biz_no,omitempty"`                   // 否 业务ID
-		BuyerID           string  `json:"buyer_id,omitempty" url:"buyer_id,omitempty"`                       // 否 买家帐号
-		BuyerLoginID      string  `json:"buyer_login_id,omitempty" url:"buyer_login_id,omitempty"`           // 否 买家帐号
-		SellerID          string  `json:"seller_id,omitempty" url:"seller_id,omitempty"`                     // 否 卖家帐号
-		SellerEmail       string  `json:"seller_email,omitempty" url:"seller_email,omitempty"`               // 否 卖家邮箱
-		TradeStatus       string  `json:"trade_status,omitempty" url:"trade_status,omitempty"`               // 否 交易状态
-		TotalAmount       float64 `json:"total_amount,omitempty" url:"total_amount,omitempty"`               // 否 订单金额
-		ReceiptAmount     float64 `json:"receipt_amount,omitempty" url:"receipt_amount,omitempty"`           // 否 实收金额
-		InvoiceAmount     float64 `json:"invoice_amount,omitempty" url:"invoice_amount,omitempty"`           // 否 开票金额
-		BuyerPayAmount    float64 `json:"buyer_pay_amount,omitempty" url:"buyer_pay_amount,omitempty"`       // 否 付款金额
-		PointAmount       float64 `json:"point_amount,omitempty" url:"point_amount,omitempty"`               // 否 积分金额
-		RefundFee         float64 `json:"refund_fee,omitempty" url:"refund_fee,omitempty"`                   // 否 退款金额
-		Subject           string  `json:"subject,omitempty" url:"subject,omitempty"`                         // 是 标题
-		Body              string  `json:"body,omitempty" url:"body,omitempty"`                               // 否 描述
-		GmtCreate         string  `json:"gmt_create,omitempty" url:"gmt_create,omitempty"`                   // 否 创建时间
-		GmtPayment        string  `json:"gmt_payment,omitempty" url:"gmt_payment,omitempty"`                 // 否 付款时间
-		GmtRefund         string  `json:"gmt_refund,omitempty" url:"gmt_refund,omitempty"`                   // 否 退款时间
-		GmtClose          string  `json:"gmt_close,omitempty" url:"gmt_close,omitempty"`                     // 否 结束时间
-		FundBillList      string  `json:"fund_bill_list,omitempty" url:"fund_bill_list,omitempty"`           // 否 渠道金额
-		PassbackParams    string  `json:"passback_params,omitempty" url:"passback_params,omitempty"`         // 否 回传参数
-		VoucherDetailList string  `json:"voucher_detail_list,omitempty" url:"voucher_detail_list,omitempty"` // 否 优惠券
+		NotifyTime        string `json:"notify_time,omitempty" url:"notify_time,omitempty"`                 // 是 时间
+		NotifyType        string `json:"notify_type,omitempty" url:"notify_type,omitempty"`                 // 是 类型
+		NotifyID          string `json:"notify_id,omitempty" url:"notify_id,omitempty"`                     // 是 校验ID
+		APPID             string `json:"appid,omitempty" url:"appid,omitempty"`                             // 是 应用ID
+		Charset           string `json:"charset,omitempty" url:"charset,omitempty"`                         // 是 utf-8
+		Version           string `json:"version,omitempty" url:"version,omitempty"`                         // 是 1.0
+		SignType          string `json:"sign_type,omitempty" url:"-"`                                       // 是 RSA2
+		Sign              string `json:"sign,omitempty" url:"-"`                                            // 是 签名
+		TradeNo           string `json:"trade_no,omitempty" url:"trade_no,omitempty"`                       // 是 交易号
+		OutTradeNo        string `json:"out_trade_no,omitempty" url:"out_trade_no,omitempty"`               // 是 单号
+		OutBizNo          string `json:"out_biz_no,omitempty" url:"out_biz_no,omitempty"`                   // 否 业务ID
+		BuyerID           string `json:"buyer_id,omitempty" url:"buyer_id,omitempty"`                       // 否 买家帐号
+		BuyerLoginID      string `json:"buyer_login_id,omitempty" url:"buyer_login_id,omitempty"`           // 否 买家帐号
+		SellerID          string `json:"seller_id,omitempty" url:"seller_id,omitempty"`                     // 否 卖家帐号
+		SellerEmail       string `json:"seller_email,omitempty" url:"seller_email,omitempty"`               // 否 卖家邮箱
+		TradeStatus       string `json:"trade_status,omitempty" url:"trade_status,omitempty"`               // 否 交易状态
+		TotalAmount       string `json:"total_amount,omitempty" url:"total_amount,omitempty"`               // 否 订单金额
+		ReceiptAmount     string `json:"receipt_amount,omitempty" url:"receipt_amount,omitempty"`           // 否 实收金额
+		InvoiceAmount     string `json:"invoice_amount,omitempty" url:"invoice_amount,omitempty"`           // 否 开票金额
+		BuyerPayAmount    string `json:"buyer_pay_amount,omitempty" url:"buyer_pay_amount,omitempty"`       // 否 付款金额
+		PointAmount       string `json:"point_amount,omitempty" url:"point_amount,omitempty"`               // 否 积分金额
+		RefundFee         string `json:"refund_fee,omitempty" url:"refund_fee,omitempty"`                   // 否 退款金额
+		Subject           string `json:"subject,omitempty" url:"subject,omitempty"`                         // 是 标题
+		Body              string `json:"body,omitempty" url:"body,omitempty"`                               // 否 描述
+		GmtCreate         string `json:"gmt_create,omitempty" url:"gmt_create,omitempty"`                   // 否 创建时间
+		GmtPayment        string `json:"gmt_payment,omitempty" url:"gmt_payment,omitempty"`                 // 否 付款时间
+		GmtRefund         string `json:"gmt_refund,omitempty" url:"gmt_refund,omitempty"`                   // 否 退款时间
+		GmtClose          string `json:"gmt_close,omitempty" url:"gmt_close,omitempty"`                     // 否 结束时间
+		FundBillList      string `json:"fund_bill_list,omitempty" url:"fund_bill_list,omitempty"`           // 否 渠道金额
+		PassbackParams    string `json:"passback_params,omitempty" url:"passback_params,omitempty"`         // 否 回传参数
+		VoucherDetailList string `json:"voucher_detail_list,omitempty" url:"voucher_detail_list,omitempty"` // 否 优惠券
 	}
 	// Query query
 	Query struct {
